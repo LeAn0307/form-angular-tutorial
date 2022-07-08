ca6a01f1a982a1789ed7c568b340503a56923709
An <lethixuanan2012@gmail.com>
2022-07-08 15:34:01 +0700

    init

---
+++ form-angular/src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import{Hero} from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent implements OnInit {
+
+  powers =['Really Smart', 'Super Flexible','Super Hot', 'Weather Changer'];
+  model=new Hero (18, 'DrIQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+  onSubmit(){this.submitted=true;}
+
+
+
+  constructor() { }
+  ngOnInit(): void {
+    const myHero =  new Hero(42, 'SkyDog',  'Fetch any object at any distance',  'Leslie Rollover');
+    console.log('My hero is called ' + myHero.name); // "My hero is called SkyDog
+  }
+
+}
